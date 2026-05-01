5973c4cd37635f8d8e72d653add4c2657f17690a
sean <dltjdrnjs3@naver.com>
2026-05-01 21:21:57 +0900

    Create PickStrangerEnv.kt

---
+++ src/main/kotlin/com/example/pickstranger/env/PickStrangerEnv.kt
@@ -0,0 +1,17 @@
+package com.example.pickstranger.env
+
+import com.example.pickstranger.RevEnvironment
+
+object PickStrangerEnv {
+    @JvmSynthetic
+    fun create(): RevEnvironment.Builder {
+        return RevEnvironment.newBuilder()
+            .setBackendServiceUrl("http://localhost:8080")
+            .setProxyPort(9090)
+            .setAiModuleGrpcUrl("http://lcalhost:5050")
+            .setRiskScoreThreshold(0.5f)
+            .setAiResponseTimeout(5000)
+            .setBlacklistDuration(5000)
+            .setVersion("v1.0.0")
+    }
+}
